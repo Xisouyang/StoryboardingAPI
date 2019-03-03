696c0965a3d910db9254e157ed847a41320567d6
Stephen Ouyang <xisouyang@gmail.com>
2019-03-03 13:38:20 -0800

    init

---
+++ server.js
@@ -0,0 +1,18 @@
+// express framework for node server
+const express = require('express')
+const app = express()
+
+// use handlebars for views
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// routes
+app.use('/', require('./routes/index'))
+
+// start up server
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log("listening on port 3000")
+})
